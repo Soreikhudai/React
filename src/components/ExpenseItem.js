d6d1038f8ad9d1b43d73747e32723bf233779248
Soreikhudai <khudaisoreipam@gmail.com>
2023-02-04 23:36:32 +0530

    updated ok done

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+
+function ExpenseItem() {
+    return (
+        <div>
+            <h2> just hard code for now</h2>
+            <div>
+                <div>Food</div>
+                <div>Rs 10</div>
+            </div>
+            <div>
+                <div>Petrol</div>
+                <div>RS 100</div>
+            </div>
+            <div>
+                <div>Movies</div>
+                <div>RS 200</div>
+            </div>
+        </div>
+    )
+}
+export default ExpenseItem;
