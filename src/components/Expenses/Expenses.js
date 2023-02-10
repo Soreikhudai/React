70de0dab624c9923c869744c1c65c983e3223553
Soreikhudai <khudaisoreipam@gmail.com>
2023-02-10 14:13:11 +0530

    filtered assignment done

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,13 +8,16 @@ const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2019')
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear)
-
     }
+    const filterExpenses= props.items.filter(expense=>{
+        return expense.date.getFullYear().toString()===filteredYear;
+    })
+
     return (
         <div>
             <Card className="expenses">
                 <ExpenseFilter onChangeFilter={filterChangeHandler} selected={filteredYear} />
-                {props.items.map((expense) => (
+                {filterExpenses.map((expense) => (
                     <ExpenseItem
                         key={expense.id}
                         title={expense.title}
